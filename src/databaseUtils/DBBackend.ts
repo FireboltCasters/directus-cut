69c4c71564845fe053893a74ac07f1c6ddfc95e4
Lint Action <lint-action@samuelmeuli.com>
2022-02-15 16:22:33 +0000

    Fix code style issues with Prettier

--- src/databaseUtils/DBBackend.ts
+++ src/databaseUtils/DBBackend.ts
@@ -1,42 +1,53 @@
-import DBImplementation from "./DBImplementation";
-
+import DBImplementation from './DBImplementation';
 
 export default class DBBackend implements DBImplementation {
+  private database: any;
 
-    private database: any;
+  constructor(database: any) {
+    this.database = database;
+  }
 
-    constructor(database: any) {
-        this.database = database;
-    }
+  async createOne(collection: string, data: any): Promise<any> {
+    return await this.database(collection).insert(data).returning('*');
+  }
 
-    async createOne(collection: string, data: any): Promise<any> {
-        return await this.database(collection).insert(data).returning("*");
-    }
+  async deleteOne(
+    collection: string,
+    primaryKey: number | string
+  ): Promise<boolean> {
+    await this.database(collection).where({id: primaryKey}).del();
+    return true;
+  }
 
-    async deleteOne(collection: string, primaryKey: number | string): Promise<boolean> {
-        await this.database(collection).where({id: primaryKey}).del();
-        return true;
-    }
+  async getCurrentUserRole(): Promise<string> {
+    return Promise.resolve('');
+  }
 
-    async getCurrentUserRole(): Promise<string> {
-        return Promise.resolve("");
-    }
+  async isCurrentUserAdmin(): Promise<boolean> {
+    return Promise.resolve(false);
+  }
 
-    async isCurrentUserAdmin(): Promise<boolean> {
-        return Promise.resolve(false);
-    }
+  async readMany(
+    collection: string,
+    query?: {offset?: any; limit?: any; meta?: any; filter?: any}
+  ): Promise<any> {
+    // TODO: Transform query into knex syntax (https://knexjs.org/#Builder-where)
+    //return await this.database(collection).where({id: primaryKey});
+    return Promise.resolve(undefined);
+  }
 
-    async readMany(collection: string, query?: { offset?: any; limit?: any; meta?: any; filter?: any }): Promise<any> {
-        // TODO: Transform query into knex syntax (https://knexjs.org/#Builder-where)
-        //return await this.database(collection).where({id: primaryKey});
-        return Promise.resolve(undefined);
-    }
+  async readOne(collection: string, primaryKey: number | string): Promise<any> {
+    return await this.database(collection).where({id: primaryKey}).first();
+  }
 
-    async readOne(collection: string, primaryKey: number | string): Promise<any> {
-        return await this.database(collection).where({id: primaryKey}).first();
-    }
-
-    async updateOne(collection: string, primaryKey: number | string, data: any): Promise<any> {
-        return await this.database(collection).where({id: primaryKey}).update(data).returning("*");
-    }
+  async updateOne(
+    collection: string,
+    primaryKey: number | string,
+    data: any
+  ): Promise<any> {
+    return await this.database(collection)
+      .where({id: primaryKey})
+      .update(data)
+      .returning('*');
+  }
 }
--- src/databaseUtils/DBImplementation.ts
+++ src/databaseUtils/DBImplementation.ts
@@ -1,18 +1,22 @@
-
-
 export default interface DBImplementation {
+  createOne(collection: string, data: any): Promise<any>;
 
-    createOne(collection: string, data: any): Promise<any>;
+  readOne(collection: string, primaryKey: number | string): Promise<any>;
 
-    readOne(collection: string, primaryKey: number | string): Promise<any>;
+  readMany(
+    collection: string,
+    query?: {offset?: any; limit?: any; meta?: any; filter?: any}
+  ): Promise<any>;
 
-    readMany(collection: string, query?: { offset?: any, limit?: any, meta?: any, filter?: any }): Promise<any>;
+  updateOne(
+    collection: string,
+    primaryKey: number | string,
+    data: any
+  ): Promise<any>;
 
-    updateOne(collection: string, primaryKey: number | string, data: any): Promise<any>;
+  deleteOne(collection: string, primaryKey: number | string): Promise<boolean>;
 
-    deleteOne(collection: string, primaryKey: number | string): Promise<boolean>;
+  getCurrentUserRole(): Promise<string>;
 
-    getCurrentUserRole(): Promise<string>;
-
-    isCurrentUserAdmin(): Promise<boolean>;
+  isCurrentUserAdmin(): Promise<boolean>;
 }
