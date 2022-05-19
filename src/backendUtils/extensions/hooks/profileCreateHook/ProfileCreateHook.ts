114e44598700a0f0f91540b7b0be81ea0a162f11
Nils <nilsbaumgartner1994@gmail.com>
2022-05-19 10:32:29 +0200

    Fix merge problems ...

--- src/backendUtils/extensions/hooks/profileCreateHook/ProfileCreateHook.ts
+++ src/backendUtils/extensions/hooks/profileCreateHook/ProfileCreateHook.ts
@@ -1,21 +1,15 @@
-import TypeSpecificRegisterFunctions from '../../../helper/typeInterfaces/TypeSpecificRegisterFunctions';
-import RegisterFunctionContext from '../../../helper/typeInterfaces/RegisterFunctionContext';
-import EventHelper from '../EventHelper';
+import TypeSpecificRegisterFunctions from "../../../helper/typeInterfaces/TypeSpecificRegisterFunctions";
+import RegisterFunctionContext from "../../../helper/typeInterfaces/RegisterFunctionContext";
+import EventHelper from "../EventHelper";
 
 /**
  * A small hook, to create a profile upon user registration
  * You will need to create a table (e. G. "profiles") with a field "user" which relates to "directus_users"
  */
 
-const DEFAULT_PROFILE_TABLENAME: string = "profiles"
+const DEFAULT_PROFILE_TABLENAME = "profiles"
 
 export default class ProfileCreateHook {
-  static handleHook(
-    tablename_profiles,
-    registerFunctions: TypeSpecificRegisterFunctions,
-    context: RegisterFunctionContext
-  ) {
-    const {filter, action, init, schedule} = registerFunctions;
 
     static handleHook(
         tablename_profiles: string,
@@ -41,18 +35,20 @@ export default class ProfileCreateHook {
                         );
                     }
 
-        let profile = {
-          user: userId,
-          status: status_published,
-        };
-        await database(tablename_profiles).insert(profile);
-      } else {
-        //                        console.log(existingProfile);
-      }
+                    const existingProfile = await database(tablename_profiles)
+                        .where({user: userId})
+                        .first(); //get user
+                    if (!existingProfile) {
+                        const status_published = 'published';
 
-      return input;
-    });
-  }
+                        let profile = {
+                            user: userId,
+                            status: status_published,
+                        };
+                        await database(tablename_profiles).insert(profile);
+                    } else {
+//                        console.log(existingProfile);
+                    }
 
                     return input;
                 }
@@ -64,9 +60,9 @@ export default class ProfileCreateHook {
      * Register the profile create hook
      * @param customProfileTablename you can specify the name of the profile tablename
      */
-    static registerHook(customProfileTablename?: string)
+    static registerHook(customProfileTablename?: any)
     {
-        let tablename: string = customProfileTablename || DEFAULT_PROFILE_TABLENAME;
+        let tablename = customProfileTablename || DEFAULT_PROFILE_TABLENAME;
         return ProfileCreateHook.handleHook.bind(null, tablename);
     }
 
